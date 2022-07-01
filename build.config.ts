6501d2fd0c8d1257d3215ee932a1fc69ca807dea
duowenbo <duowenbo@hatach.com.cn>
2022-07-01 14:55:25 +0800

    chore: update dependencies

--- build.config.ts
+++ build.config.ts
@@ -2,7 +2,7 @@ import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
   entries: ["./src/index"],
-  externals: ["conventional-changelog"],
+  // externals: ["conventional-changelog"],
   clean: true,
   declaration: false,
   rollup: {
