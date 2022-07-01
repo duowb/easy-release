de9a187b395b1f497ad4adb2a477137b65d1774e
duowenbo <duowenbo@hatach.com.cn>
2022-07-01 10:27:24 +0800

    chore: clean package

--- build.config.ts
+++ build.config.ts
@@ -1,16 +1,12 @@
-import { defineBuildConfig } from 'unbuild'
+import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
-  entries: [
-    "./src/index"
-  ],
-  externals: [
-    'chalk', 'semver', 'enquirer', 'execa', 'yargs', 'conventional-changelog'
-  ],
+  entries: ["./src/index"],
+  externals: ["conventional-changelog"],
   clean: true,
   declaration: false,
   rollup: {
     emitCJS: true,
     inlineDependencies: true,
   },
-})
+});
