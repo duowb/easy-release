1e4a208a908b1164e934ec3b2b18be9ff5c5d9bc
doubledKill <double1010@foxmail.com>
2023-06-08 15:12:30 +0800

    feat: add check package-lock publish

--- src/publish.ts
+++ src/publish.ts
@@ -1,12 +1,12 @@
 import { log, logColor } from "./log";
 import { PackageInfo } from "./types";
 import { args } from "./command";
-import { runIfNotDry, step } from "./utils";
+import { checkNpmPackageManage, runIfNotDry, step } from "./utils";
 
 export async function publishPackage({ name, version }: PackageInfo) {
   // publish packages
   step("Publishing packages...");
-  let releaseTag = '';
+  let releaseTag = "";
   if (args.tag) {
     releaseTag = args.tag;
   } else if (version.includes("alpha")) {
@@ -16,10 +16,11 @@ export async function publishPackage({ name, version }: PackageInfo) {
   } else if (version.includes("rc")) {
     releaseTag = "rc";
   }
-
+  const packageManage = checkNpmPackageManage();
+  log(`use ${packageManage} publish ${name}@${version}`, logColor.FgGreen);
   try {
     const tag = releaseTag ? `--tag ${releaseTag}` : "";
-    await runIfNotDry(`npm publish ${tag}`, { stdio: "pipe" });
+    await runIfNotDry(`${packageManage} publish ${tag}`, { stdio: "pipe" });
     log(`Successfully published ${name}@${version}`, logColor.FgGreen);
   } catch (error) {
     log(`${name} fail published. [error]${error}`, logColor.FgRed);
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,21 @@
 import { execaCommand } from "execa";
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync, existsSync } from "fs";
 import path from "path";
 import { args } from "./command";
 import { logColor, log } from "./log";
 import { PackageInfo } from "./types";
 
+type PackageManage = "npm" | "pnpm" | "yarn" | "bun";
+export const LOCKS: Record<string, PackageManage> = {
+  "bun.lockb": "bun",
+  "pnpm-lock.yaml": "pnpm",
+  "yarn.lock": "yarn",
+  "package-lock.json": "npm",
+  "npm-shrinkwrap.json": "npm",
+};
+
 const isDryRun = args.dry;
+console.log("🚀 ~ file: utils.ts:18 ~ isDryRun:", isDryRun)
 export const run = (command: string, opts = {}) =>
   execaCommand(command, { stdio: "inherit", ...opts });
 
@@ -42,4 +52,16 @@ export function updatePkgInfo(version: string) {
   const pkg = JSON.parse(readFileSync(pkgPath, "utf-8"));
   pkg.version = version;
   writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
+}
+
+export function checkNpmPackageManage() {
+  const cwd = process.cwd();
+  for (const lock in LOCKS) {
+    const lockPath = path.resolve(cwd, lock);
+    const isExists = existsSync(lockPath);
+    if (isExists) {
+      return LOCKS[lock]
+    }
+  }
+  return 'npm'
 }
